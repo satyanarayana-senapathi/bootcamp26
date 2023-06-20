4df6092fb9551feb57c29d8e92e718b96979892f
satyanarayana-zemoso <satyanarayana.senapathi@zemosolabs.com>
2023-06-20 12:47:34 +0530

    changed ec2 ip

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     }),
     new EnvironmentPlugin({
       REACT_APP_API_ENDPOINT: JSON.stringify(
-        process.env.REACT_APP_API_ENDPOINT || 'http://34.205.77.110'
+        process.env.REACT_APP_API_ENDPOINT || 'http://3.92.186.125'
       ),
     }),
   ],
